63ccc901c7606eda3ddb659321b2aa09e5d1e354
Mitchell <198377+mitchell-fyi@users.noreply.github.com>
2023-03-09 16:18:42 +0000

    add csv to json data

--- src/app/services/influencers/controller.ts
+++ src/app/services/influencers/controller.ts
@@ -1,5 +1,12 @@
 import type { Influencer } from './types';
+import path from 'path';
+import csv from 'csvtojson';
+
+const CSV_DATA_PATH = path.join(__dirname, './data/instagram_influencers.csv');
+const data = async () => await csv().fromFile(CSV_DATA_PATH);
 
 export const findAll = async (): Promise<Influencer[]> => {
-  return [];
+  const items = await data();
+
+  return items;
 };
